90c184ea1cb0156b06a971e4d141dec412608662
Apollo3zehn <vwilms@outlook.com>
2018-06-27 19:20:22 +0200

    Partial commit.

--- S1_Framework/Serialization.py
+++ S1_Framework/Serialization.py
@@ -1,3 +1,5 @@
+import inspect
+
 from . import Convention
 
 class Dynamic:
@@ -10,8 +12,8 @@ class Dynamic:
 
             if isinstance(value, dict):
                 setattr(self, key, Dynamic(value))
-
-            if isinstance(value, list):
+                
+            elif isinstance(value, list):
                 convertedValue = [Dynamic(element) if isinstance(element, dict) else element for element in value]
                 setattr(self, key, convertedValue)
 
@@ -20,3 +22,57 @@ class Dynamic:
 
 def ToDynamic(data):
     return Dynamic(data)
+
+def Deserialize(data, cls):
+    
+    annotations: list = GetInheritedAnnotations(cls)
+
+    if issubclass(cls, list):
+
+        listType = cls.__args__[0]
+        instance: list = list()
+
+        for value in data:
+            instance.append(Deserialize(value, listType))
+
+        return instance
+        
+    elif issubclass(cls, dict):
+
+        keyType = cls.__args__[0]
+        valueType = cls.__args__[1]
+
+        instance: dict = dict()
+
+        for key, value in data.items():
+
+            key = Convention.KebapCaseToPascalCase(key)
+
+            instance.update(Deserialize(key, keyType), Deserialize(value, valueType))
+
+        return instance
+        
+    else:
+
+        instance: cls = cls()
+
+        for name, value in data.items():
+            
+            name = Convention.KebapCaseToPascalCase(name)
+            fieldType = annotations.get(name)
+
+            if inspect.isclass(fieldType) and isinstance(value, (dict, tuple, list, set, frozenset)):
+                setattr(instance, name, Deserialize(value, fieldType))
+            else:
+                setattr(instance, name, value)
+
+        return instance
+
+def GetAnnotations(cls):
+    return cls.__annotations__ if hasattr(cls, '__annotations__') else dict()
+
+def GetInheritedAnnotations(cls):
+    
+    baseTypeSet = inspect.getmro(cls)
+    
+    return dict([annotation for baseType in baseTypeSet for annotation in GetAnnotations(baseType).items()])
--- S3_Test/PowerPerformanceTests.py
+++ S3_Test/PowerPerformanceTests.py
@@ -6,18 +6,20 @@ from S1_Framework import Serialization
 from S2_Internal import PowerPerformance
 
 # https://code.visualstudio.com/docs/languages/json#_mapping-in-the-user-settings
+# https://www.deine-berge.de/Rechner/Koordinaten/Dezimal/51.525904,8.607483
 
 def GeneralTest():
 
     logger = logging.getLogger("PowerPerformanceTests")
     logging.basicConfig(level=logging.INFO)
 
-    filePath = os.path.join(os.path.dirname(os.path.realpath(__file__)), r".\TestData\data.site-assessment.json")
+    filePath = os.path.join(os.path.dirname(os.path.realpath(__file__)), r".\TestData\data.site-assessment.json")   
 
     with open(filePath) as fileStream:
         data = json.load(fileStream)
-        
-    configuration = Serialization.ToDynamic(data)
+    
+    # configuration = Serialization.ToDynamic(data)
+    configuration = Serialization.Deserialize(data, PowerPerformance.SiteAssessment.Configuration)
     PowerPerformance.SiteAssessment.Calculate(configuration, logger)
 
     raise Exception()
